e58672fc0f2bdc7c439ad36e64ac379c1d2a7c37
utkuufuk <utkuufuk@gmail.com>
2017-05-04 17:31:30 +0300

    polishing

--- CUDA/HW3/student_func.cu
+++ CUDA/HW3/student_func.cu
@@ -1,64 +1,3 @@
-/* Udacity Homework 3
-   HDR Tone-mapping
-  Background HDR
-  ==============
-  A High Dynamic Range (HDR) image contains a wider variation of intensity
-  and color than is allowed by the RGB format with 1 byte per channel that we
-  have used in the previous assignment.  
-  To store this extra information we use single precision floating point for
-  each channel.  This allows for an extremely wide range of intensity values.
-  In the image for this assignment, the inside of church with light coming in
-  through stained glass windows, the raw input floating point values for the
-  channels range from 0 to 275.  But the mean is .41 and 98% of the values are
-  less than 3!  This means that certain areas (the windows) are extremely bright
-  compared to everywhere else.  If we linearly map this [0-275] range into the
-  [0-255] range that we have been using then most values will be mapped to zero!
-  The only thing we will be able to see are the very brightest areas - the
-  windows - everything else will appear pitch black.
-  The problem is that although we have cameras capable of recording the wide
-  range of intensity that exists in the real world our monitors are not capable
-  of displaying them.  Our eyes are also quite capable of observing a much wider
-  range of intensities than our image formats / monitors are capable of
-  displaying.
-  Tone-mapping is a process that transforms the intensities in the image so that
-  the brightest values aren't nearly so far away from the mean.  That way when
-  we transform the values into [0-255] we can actually see the entire image.
-  There are many ways to perform this process and it is as much an art as a
-  science - there is no single "right" answer.  In this homework we will
-  implement one possible technique.
-  Background Chrominance-Luminance
-  ================================
-  The RGB space that we have been using to represent images can be thought of as
-  one possible set of axes spanning a three dimensional space of color.  We
-  sometimes choose other axes to represent this space because they make certain
-  operations more convenient.
-  Another possible way of representing a color image is to separate the color
-  information (chromaticity) from the brightness information.  There are
-  multiple different methods for doing this - a common one during the analog
-  television days was known as Chrominance-Luminance or YUV.
-  We choose to represent the image in this way so that we can remap only the
-  intensity channel and then recombine the new intensity values with the color
-  information to form the final image.
-  Old TV signals used to be transmitted in this way so that black & white
-  televisions could display the luminance channel while color televisions would
-  display all three of the channels.
-  
-  Tone-mapping
-  ============
-  In this assignment we are going to transform the luminance channel (actually
-  the log of the luminance, but this is unimportant for the parts of the
-  algorithm that you will be implementing) by compressing its range to [0, 1].
-  To do this we need the cumulative distribution of the luminance values.
-  Example
-  -------
-  input : [2 4 3 3 1 7 4 5 7 0 9 4 3 2]
-  min / max / range: 0 / 9 / 9
-  histo with 3 bins: [4 7 3]
-  cdf : [4 11 14]
-  Your task is to calculate this cumulative distribution by following these
-  steps.
-*/
-
 #include <limits.h>
 #include <float.h>
 #include <math.h>
@@ -66,14 +5,14 @@
 
 #include "utils.h"
 
-#define THREADS_PER_BLOCK 1024
-#define NUM_EXAMPLES 100
-#define MIN 0
-#define MAX 1
+const int THREADS_PER_BLOCK = 1024;
+const int NUM_EXAMPLES = 100;
+const int MIN = 0;
+const int MAX = 1;
 
 __global__
-void histogramKernel(unsigned int* deviceBins, 
-                     const float* deviceInBuffer, 
+void histogram(unsigned int* d_bins, 
+                     const float* d_outBuffer, 
                      const int binCount, 
                      const float lumMin, 
                      const float lumMax, 
@@ -87,12 +26,12 @@ void histogramKernel(unsigned int* deviceBins,
         return;
     }
     float range = lumMax - lumMin;
-    int binIndex = ((deviceInBuffer[bufferIndex] - lumMin) / range) * binCount;
-    atomicAdd(&deviceBins[binIndex], 1);
+    int binIndex = ((d_outBuffer[bufferIndex] - lumMin) / range) * binCount;
+    atomicAdd(&d_bins[binIndex], 1);
 }
 
 __global__ 
-void scanKernel(unsigned int* deviceBins, int bufferSize) 
+void scan(unsigned int* d_bins, int bufferSize) 
 {
     int bufferIndex = threadIdx.x + blockDim.x * blockIdx.x;
     
@@ -108,25 +47,25 @@ void scanKernel(unsigned int* deviceBins, int bufferSize)
 
         if (spot >= 0)
         {
-          val = deviceBins[spot];
+          val = d_bins[spot];
         }
         __syncthreads();
 
         if (spot >= 0)
         {
-          deviceBins[bufferIndex] += val;
+          d_bins[bufferIndex] += val;
         }
         __syncthreads();
     }
 }
 
 __global__
-void reduceMinMaxKernel(const float* const deviceInBuffer, 
-                        float* deviceOutBuffer, 
+void reduce(const float* const d_inBuffer, 
+                        float* d_outBuffer, 
                         const size_t bufferSize, 
                         int minOrMax) 
 {
-    extern __shared__ float sharedBuffer[];
+    extern __shared__ float sharedBuf[];
     
     int blockSize = blockDim.x;
     int blockIndex = blockIdx.x;
@@ -136,18 +75,18 @@ void reduceMinMaxKernel(const float* const deviceInBuffer,
     // copy this block's portion of the input buffer into the shared buffer
     if (bufferIndex < bufferSize) 
     {
-        sharedBuffer[threadIndex] = deviceInBuffer[bufferIndex];
+        sharedBuf[threadIndex] = d_inBuffer[bufferIndex];
     } 
     else
     {
         // handle out-of-bounds situations
         if (minOrMax == MIN)
         {
-            sharedBuffer[threadIndex] = FLT_MAX;
+            sharedBuf[threadIndex] = FLT_MAX;
         }
         else
         {
-            sharedBuffer[threadIndex] = -FLT_MAX;
+            sharedBuf[threadIndex] = -FLT_MAX;
         }
     }
     __syncthreads();
@@ -159,11 +98,11 @@ void reduceMinMaxKernel(const float* const deviceInBuffer,
         {
             if (minOrMax == MIN) 
             {
-                sharedBuffer[threadIndex] = min(sharedBuffer[threadIndex], sharedBuffer[threadIndex + s]);
+                sharedBuf[threadIndex] = min(sharedBuf[threadIndex], sharedBuf[threadIndex + s]);
             } 
             else 
             {
-                sharedBuffer[threadIndex] = max(sharedBuffer[threadIndex], sharedBuffer[threadIndex + s]);
+                sharedBuf[threadIndex] = max(sharedBuf[threadIndex], sharedBuf[threadIndex + s]);
             }
         }
         __syncthreads();
@@ -172,7 +111,7 @@ void reduceMinMaxKernel(const float* const deviceInBuffer,
     // write the min/max value of this block's portion into the output buffer
     if (threadIndex == 0) 
     {
-        deviceOutBuffer[blockIndex] = sharedBuffer[0];
+        d_outBuffer[blockIndex] = sharedBuf[0];
     }
 }
 
@@ -181,31 +120,32 @@ static int calculateNumBlocks(int bufferSize)
     return (int) ceil((float) bufferSize / (float) THREADS_PER_BLOCK) + 1;
 }
 
-static float reduceMinMax(const float* const deviceInBuffer, const size_t bufferSize, int minOrMax) 
+static float reduceMinMax(const float* const d_outBuffer, const size_t bufferSize, int minOrMax) 
 {
     size_t currentBufferSize = bufferSize;
-    float* deviceInputTemp;
-    float* deviceOutputTemp;
+    float* d_inTemp;
+    float* d_outTemp;
     
     // copy the input buffer to a temporary buffer
-    checkCudaErrors(cudaMalloc(&deviceInputTemp, sizeof(float) * bufferSize));    
-    checkCudaErrors(cudaMemcpy(deviceInputTemp, deviceInBuffer, sizeof(float) * bufferSize, cudaMemcpyDeviceToDevice));
+    size_t bufferMemSize = sizeof(float) * bufferSize; 
+    checkCudaErrors(cudaMalloc(&d_inTemp, bufferMemSize));    
+    checkCudaErrors(cudaMemcpy(d_inTemp, d_outBuffer, bufferMemSize, cudaMemcpyDeviceToDevice));
     
-    const int sharedMemorySize = sizeof(float) * THREADS_PER_BLOCK;
+    size_t sharedMemorySize = sizeof(float) * THREADS_PER_BLOCK;
     
     // keep reducing until the entire thing fits into a single block
     while (1) 
     {
         int numBlocks = calculateNumBlocks(currentBufferSize);
-        checkCudaErrors(cudaMalloc(&deviceOutputTemp, sizeof(float) * numBlocks));
-        reduceMinMaxKernel<<<numBlocks, THREADS_PER_BLOCK, sharedMemorySize>>>
-                            (deviceInputTemp, deviceOutputTemp, currentBufferSize, minOrMax);
+        checkCudaErrors(cudaMalloc(&d_outTemp, sizeof(float) * numBlocks));
+        reduce<<<numBlocks, THREADS_PER_BLOCK, sharedMemorySize>>>
+              (d_inTemp, d_outTemp, currentBufferSize, minOrMax);
         cudaDeviceSynchronize(); 
         checkCudaErrors(cudaGetLastError());
             
         // move the current input to the output, and clear the last input if necessary
-        checkCudaErrors(cudaFree(deviceInputTemp));
-        deviceInputTemp = deviceOutputTemp;
+        checkCudaErrors(cudaFree(d_inTemp));
+        d_inTemp = d_outTemp;
         
         if (currentBufferSize < THREADS_PER_BLOCK) 
         {
@@ -219,14 +159,14 @@ static float reduceMinMax(const float* const deviceInBuffer, const size_t buffer
     }
     
     // transfer the min/max value to host memory and return
-    float hostResult;
-    cudaMemcpy(&hostResult, deviceOutputTemp, sizeof(float), cudaMemcpyDeviceToHost);
-    cudaFree(deviceOutputTemp);
-    return hostResult;
+    float h_result;
+    cudaMemcpy(&h_result, d_outTemp, sizeof(float), cudaMemcpyDeviceToHost);
+    cudaFree(d_outTemp);
+    return h_result;
 }
 
-void your_histogram_and_prefixsum(const float* const deviceLogLum,
-                                  unsigned int* const deviceCdf,
+void your_histogram_and_prefixsum(const float* const d_logLum,
+                                  unsigned int* const d_cdf,
                                   float &minLogLum,
                                   float &maxLogLum,
                                   const size_t numRows,
@@ -235,46 +175,46 @@ void your_histogram_and_prefixsum(const float* const deviceLogLum,
 {
     // REDUCE
     const size_t imageSize = numRows * numCols;
-    minLogLum = reduceMinMax(deviceLogLum, imageSize, 0);
-    maxLogLum = reduceMinMax(deviceLogLum, imageSize, 1);
+    minLogLum = reduceMinMax(d_logLum, imageSize, 0);
+    maxLogLum = reduceMinMax(d_logLum, imageSize, 1);
     
     printf("Min: %f\n", minLogLum);
     printf("Max: %f\n", maxLogLum);
     printf("Number of Bins: %d\n", numBins);
     
     // HISTOGRAM
-    unsigned int* deviceBins;
+    unsigned int* d_bins;
     size_t histogramSize = sizeof(unsigned int) * numBins;
-    checkCudaErrors(cudaMalloc(&deviceBins, histogramSize));    
-    checkCudaErrors(cudaMemset(deviceBins, 0, histogramSize));
+    checkCudaErrors(cudaMalloc(&d_bins, histogramSize));    
+    checkCudaErrors(cudaMemset(d_bins, 0, histogramSize));
     dim3 numBlocksHistogram(calculateNumBlocks(imageSize));
-    histogramKernel<<<numBlocksHistogram, THREADS_PER_BLOCK>>>
-                     (deviceBins, deviceLogLum, numBins, minLogLum, maxLogLum, imageSize);
+    histogram<<<numBlocksHistogram, THREADS_PER_BLOCK>>>
+                (d_bins, d_logLum, numBins, minLogLum, maxLogLum, imageSize);
     cudaDeviceSynchronize(); 
     checkCudaErrors(cudaGetLastError());
 
     // display some examples of histogram
-    unsigned int hostOutBuffer[NUM_EXAMPLES];
-    cudaMemcpy(&hostOutBuffer, deviceBins, sizeof(unsigned int) * NUM_EXAMPLES, cudaMemcpyDeviceToHost);
+    unsigned int h_outBuffer[NUM_EXAMPLES];
+    cudaMemcpy(&h_outBuffer, d_bins, sizeof(unsigned int) * NUM_EXAMPLES, cudaMemcpyDeviceToHost);
     
     for (int i = 0; i < NUM_EXAMPLES; i++)
     {
-        printf("hist out %d\n", hostOutBuffer[i]);
+        printf("hist out %d\n", h_outBuffer[i]);
     }
 
     // SCAN
     dim3 numBlocksScan(calculateNumBlocks(numBins));
-    scanKernel<<<numBlocksScan, THREADS_PER_BLOCK>>>(deviceBins, numBins);
+    scan<<<numBlocksScan, THREADS_PER_BLOCK>>>(d_bins, numBins);
     cudaDeviceSynchronize(); 
     checkCudaErrors(cudaGetLastError());
     
     // display some examples of CDF
-    cudaMemcpy(&hostOutBuffer, deviceBins, sizeof(unsigned int) * NUM_EXAMPLES, cudaMemcpyDeviceToHost);
+    cudaMemcpy(&h_outBuffer, d_bins, sizeof(unsigned int) * NUM_EXAMPLES, cudaMemcpyDeviceToHost);
     
     for (int i = 0; i < NUM_EXAMPLES; i++)
     {
-        printf("cdf out %d\n", hostOutBuffer[i]);
+        printf("cdf out %d\n", h_outBuffer[i]);
     }
-    cudaMemcpy(deviceCdf, deviceBins, histogramSize, cudaMemcpyDeviceToDevice);
-    checkCudaErrors(cudaFree(deviceBins));
+    cudaMemcpy(d_cdf, d_bins, histogramSize, cudaMemcpyDeviceToDevice);
+    checkCudaErrors(cudaFree(d_bins));
 }
