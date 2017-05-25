803bb6615d67d5295459c13c6f046edbab756193
utkuufuk <utkuufuk@gmail.com>
2017-05-25 18:01:00 +0300

    bugfix in transpose code

--- CUDA/memory_efficient_transpose.cu
+++ CUDA/memory_efficient_transpose.cu
@@ -2,9 +2,8 @@
 #include "gputimer.h"
 #include "utils.h"
 
-const int N= 1024;	// matrix size will be NxN
-const int K= 32;	// TODO, set K to the correct value and tile size will be KxK
-
+const int N = 1024; // matrix size will be NxN
+const int K = 32;   // TODO, set K to the correct value and tile size will be KxK 
 
 // to be launched with one thread per element, in (tilesize)x(tilesize) threadblocks
 // thread blocks read & write tiles, in coalesced fashion
@@ -15,46 +14,46 @@ transpose_parallel_per_element_tiled(float in[], float out[])
     int in_corner_i = blockIdx.x * K;
     int in_corner_j = blockIdx.y * K;
     
-    int out_corner_j = blcokIdx.y * K;
-    int out_corner_i = blockIdx.x * K;
+    int out_corner_i = blockIdx.y * K;
+    int out_corner_j = blockIdx.x * K;
     
     int x = threadIdx.x;
     int y = threadIdx.y;
     
     __shared__ float tile[K][K];
 
-	tile[y][x] = in[(in_corner_i + x) + (in_corner_j + j) * N];
-	__syncthreads();
-	
-	out[(out_corner_i + x) + (out_corner_j + y) * N] = tile[x][y];
+    tile[y][x] = in[(in_corner_i + x) + (in_corner_j + y) * N];
+    __syncthreads();
+
+    out[(out_corner_i + x) + (out_corner_j + y) * N] = tile[x][y];
 }
 
 // The following functions and kernels are for your references
 void 
 transpose_CPU(float in[], float out[])
 {
-	for(int j=0; j < N; j++)
-    	for(int i=0; i < N; i++)
-      		out[j + i*N] = in[i + j*N]; // out(j,i) = in(i,j)
+    for(int j=0; j < N; j++)
+        for(int i=0; i < N; i++)
+            out[j + i*N] = in[i + j*N]; // out(j,i) = in(i,j)
 }
 
 // to be launched on a single thread
 __global__ void 
 transpose_serial(float in[], float out[])
 {
-	for(int j=0; j < N; j++)
-		for(int i=0; i < N; i++)
-			out[j + i*N] = in[i + j*N]; // out(j,i) = in(i,j)
+    for(int j=0; j < N; j++)
+        for(int i=0; i < N; i++)
+            out[j + i*N] = in[i + j*N]; // out(j,i) = in(i,j)
 }
 
 // to be launched with one thread per row of output matrix
 __global__ void 
 transpose_parallel_per_row(float in[], float out[])
 {
-	int i = threadIdx.x;
+    int i = threadIdx.x;
 
-	for(int j=0; j < N; j++)
-		out[j + i*N] = in[i + j*N]; // out(j,i) = in(i,j)
+    for(int j=0; j < N; j++)
+        out[j + i*N] = in[i + j*N]; // out(j,i) = in(i,j)
 }
 
 // to be launched with one thread per element, in KxK threadblocks
@@ -62,30 +61,30 @@ transpose_parallel_per_row(float in[], float out[])
 __global__ void 
 transpose_parallel_per_element(float in[], float out[])
 {
-	int i = blockIdx.x * K + threadIdx.x;
-	int j = blockIdx.y * K + threadIdx.y;
+    int i = blockIdx.x * K + threadIdx.x;
+    int j = blockIdx.y * K + threadIdx.y;
 
-	out[j + i*N] = in[i + j*N]; // out(j,i) = in(i,j)
+    out[j + i*N] = in[i + j*N]; // out(j,i) = in(i,j)
 }
 
 int main(int argc, char **argv)
 {
-	int numbytes = N * N * sizeof(float);
+    int numbytes = N * N * sizeof(float);
 
-	float *in = (float *) malloc(numbytes);
-	float *out = (float *) malloc(numbytes);
-	float *gold = (float *) malloc(numbytes);
+    float *in = (float *) malloc(numbytes);
+    float *out = (float *) malloc(numbytes);
+    float *gold = (float *) malloc(numbytes);
 
-	fill_matrix(in, N);
-	transpose_CPU(in, gold);
+    fill_matrix(in, N);
+    transpose_CPU(in, gold);
 
-	float *d_in, *d_out;
+    float *d_in, *d_out;
 
-	cudaMalloc(&d_in, numbytes);
-	cudaMalloc(&d_out, numbytes);
-	cudaMemcpy(d_in, in, numbytes, cudaMemcpyHostToDevice);
+    cudaMalloc(&d_in, numbytes);
+    cudaMalloc(&d_out, numbytes);
+    cudaMemcpy(d_in, in, numbytes, cudaMemcpyHostToDevice);
 
-	GpuTimer timer;
+    GpuTimer timer;
 
 /*  
  * Now time each kernel and verify that it produces the correct result.
@@ -96,16 +95,16 @@ int main(int argc, char **argv)
  * But this makes for messy code and our goal is teaching, not detailed benchmarking.
  */
 
-	dim3 blocks(N,N);	//TODO, you need to set the proper blocks per grid
-	dim3 threads(K,K);	//TODO, you need to set the proper threads per block
+    dim3 blocks(N / K, N / K);  //TODO, you need to set the proper blocks per grid
+    dim3 threads(K, K);     //TODO, you need to set the proper threads per block
 
-	timer.Start();
-	transpose_parallel_per_element_tiled<<<blocks,threads>>>(d_in, d_out);
-	timer.Stop();
-	cudaMemcpy(out, d_out, numbytes, cudaMemcpyDeviceToHost);
-	printf("transpose_parallel_per_element_tiled %dx%d: %g ms.\nVerifying ...%s\n", 
-		   K, K, timer.Elapsed(), compare_matrices(out, gold, N) ? "Failed" : "Success");
+    timer.Start();
+    transpose_parallel_per_element_tiled<<<blocks,threads>>>(d_in, d_out);
+    timer.Stop();
+    cudaMemcpy(out, d_out, numbytes, cudaMemcpyDeviceToHost);
+    printf("transpose_parallel_per_element_tiled %dx%d: %g ms.\nVerifying ...%s\n", 
+           K, K, timer.Elapsed(), compare_matrices(out, gold, N) ? "Failed" : "Success");
 
-	cudaFree(d_in);
-	cudaFree(d_out);
+    cudaFree(d_in);
+    cudaFree(d_out);
 }
